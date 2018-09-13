d27e9989e66c553c7d8f3cc6eec299c0567e8252
jstuart <jstuart@redhat.com>
2018-09-13 08:09:48 -0500

    renaming job add discard old branches

--- src/jobs/multiBranchCommentTrigger.groovy
+++ src/jobs/multiBranchCommentTrigger.groovy
@@ -1,12 +1,12 @@
 import org.centos.contra.jobdsl.MultiBranchJob
 
 
-jobName = 'sampleMultiBranchMerge2'
-comment = "\\[merge\\]"
+jobName = 'MultiBranchWithCommentTrigger'
+comment = "\\[test\\]"
 rOwner = 'CentOS-PaaS-SIG'
 rName = 'contra-env-sample-project'
 
 def m = new MultiBranchJob(this, jobName)
 m.addGitHub(rName, rOwner)
 m.addComment(comment)
-
+m.discardOldBranches()
