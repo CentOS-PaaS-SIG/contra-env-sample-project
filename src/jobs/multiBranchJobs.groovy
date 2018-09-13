cfebd7c66217b937a32f229b2e1a4af83e89c597
jstuart <jstuart@redhat.com>
2018-09-13 08:34:45 -0500

    adding mutlijob file

---
+++ src/jobs/multiBranchJobs.groovy
@@ -0,0 +1,24 @@
+import org.centos.contra.jobdsl.MultiBranchJob
+
+
+def jobs = [
+        'MultiBranchWithCommentTrigger': [
+                'comment': "\\[test\\]",
+                'owner': 'CentOS-PaaS-SIG',
+                'name': 'contra-env-sample-project'
+        ],
+        'BasicMutliBranchJob': [
+                'owner': 'CentOS-PaaS-SIG',
+                'name': 'ci-pipeline'
+        ]
+
+]
+
+jobs.each { name, values ->
+    def job = new MultiBranchJob(this, name)
+    job.addGitHub(values['name'], values['owner'])
+    if (values['comment']) {
+        job.addComment(values['comment'])
+    }
+    job.discardOldBranches()
+}
