0d4c686987e8c8119742c68332a13875a6ec8b82
jstuart <jstuart@redhat.com>
2018-09-14 14:24:11 -0500

    adding sample pipeline job

---
+++ src/jobs/pipelineJobs.groovy
@@ -0,0 +1,17 @@
+import org.centos.contra.jobdsl.PipelineJob
+
+
+def jobs = [
+        'samplePipelineJob': [
+                'git': ['branch': 'master', 'repoUrl': 'https://github.com/CentOS-PaaS-SIG/contra-env-sample-project.git'],
+                'ciEvent': ['name': '^kernel$']
+        ]
+]
+
+jobs.each { name, values ->
+    def job = new PipelineJob(this, name)
+    job.ciEvent(values['ciEvent'])
+    job.addGit(values['git'])
+    job.gitHubTrigger()
+    job.logRotator()
+}
