9caead062c0f28b51624a6aaed4ea0e0882415b0
jstuart <jstuart@redhat.com>
2018-09-17 15:13:06 -0500

    test ci event

--- src/jobs/pipelineJobs.groovy
+++ src/jobs/pipelineJobs.groovy
@@ -10,8 +10,7 @@ def jobs = [
 
 jobs.each { name, values ->
     def job = new PipelineJob(this, name)
-    //def jobTrigger = Triggers.fedMsgTrigger('org.fedoraproject', 'fedora-fedmsg', ['check1': 'value1'])
-    def jobTrigger = Triggers.gitHubTrigger()
+    def jobTrigger = Triggers.fedMsgTrigger('org.fedoraproject', 'fedora-fedmsg', ['check1': 'value1'])
     job.trigger(jobTrigger)
     job.addGit(values['git'])
     job.logRotate()
