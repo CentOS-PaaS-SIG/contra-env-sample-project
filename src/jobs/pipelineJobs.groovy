56fa4e41908317c6bc20a858cfd8b67a436b1b79
jstuart <jstuart@redhat.com>
2018-09-17 15:58:11 -0500

    removing loop

--- src/jobs/pipelineJobs.groovy
+++ src/jobs/pipelineJobs.groovy
@@ -1,15 +1,7 @@
 import org.centos.contra.jobdsl.PipelineJob
 
 
-def jobs = [
-        'samplePipelineJob': [
-                'git': ['branch': 'master', 'repoUrl': 'https://github.com/CentOS-PaaS-SIG/contra-env-sample-project.git'],
-        ]
-]
-
-jobs.each { name, values ->
-    def job = new PipelineJob(this, name)
-    job.fedMsgTrigger('org.fedoraprojectb', 'fedora-fedmsg', ['check1': 'value1'])
-    job.addGit(values['git'])
-    job.logRotate()
-}
+def job = new PipelineJob(this, 'samplePipelineJob')
+job.fedMsgTrigger('org.fedoraprojectb', 'fedora-fedmsg', ['check1': 'value1'])
+job.addGit('master', 'https://github.com/CentOS-PaaS-SIG/contra-env-sample-project.git')
+job.logRotate()
