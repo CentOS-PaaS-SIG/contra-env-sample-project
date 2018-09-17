6c4961e7e24ac3a90861846cf8fff456246d28c0
jstuart <jstuart@redhat.com>
2018-09-17 14:20:38 -0500

    updating pipeline job

--- src/jobs/pipelineJobs.groovy
+++ src/jobs/pipelineJobs.groovy
@@ -1,16 +1,17 @@
 import org.centos.contra.jobdsl.PipelineJob
+import org.centos.contra.jobdsl.Triggers
 
 
 def jobs = [
         'samplePipelineJob': [
                 'git': ['branch': 'master', 'repoUrl': 'https://github.com/CentOS-PaaS-SIG/contra-env-sample-project.git'],
-                'ciEvent': ['name': '^kernel$']
         ]
 ]
 
 jobs.each { name, values ->
     def job = new PipelineJob(this, name)
-    job.ciEvent(values['ciEvent'])
+    def jobTrigger = Triggers.fedMsgTrigger('org.fedoraproject', 'fedora-fedmsg', ['check1': 'value1'])
+    job.trigger(jobTrigger)
     job.addGit(values['git'])
     job.logRotator()
 }
