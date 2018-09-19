04a16d57849fd6cca9ffc449ff16f23128c08e36
jstuart <jstuart@redhat.com>
2018-09-19 11:47:30 -0500

    revert comment

--- src/jobs/multiBranchJobs.groovy
+++ src/jobs/multiBranchJobs.groovy
@@ -3,7 +3,7 @@ import org.centos.contra.jobdsl.MultiBranchJob
 
 def jobs = [
         'MultiBranchWithCommentTrigger': [
-                'comment': "\\[test1\\]",
+                'comment': "\\[test\\]",
                 'owner': 'CentOS-PaaS-SIG',
                 'name': 'contra-env-sample-project'
         ],
