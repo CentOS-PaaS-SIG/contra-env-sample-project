4b704220a764a3ff536f7c4d15893c9b6bf45209
kpavic <kpavic@redhat.com>
2018-10-08 16:15:19 +0200

    Add Jenkins CI pipeline for the sample-project

--- src/org/contra/sample/pipeline/SamplePipelineUtils.groovy
+++ src/org/contra/sample/pipeline/SamplePipelineUtils.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/groovy
 package org.contra.sample.pipeline
 
-
+import org.centos.*
 
 /**
  * Library to execute script in container
@@ -120,4 +120,82 @@ def verifyPod(String openshiftProject, String nodeName) {
             }
         }
     }
+}
+
+/**
+ * Library to set default environmental variables. Performed once at start of Jenkinsfile
+ * @param envMap: Key/value pairs which will be set as environmental variables.
+ * @return
+ */
+def setDefaultEnvVars(Map envMap=null){
+    // If we've been provided an envMap, we set env.key = value
+    // Note: This may overwrite above specified values.
+    envMap.each { key, value ->
+        env."${key.toSTring().trim()}" = value.toString().trim()
+    }
+}
+
+/**
+ * Library to set stage specific environmental variables.
+ * @param stage - Current stage
+ * @return
+ */
+def setStageEnvVars(String stage){
+    def stages =
+            ["test-contra-env-sample-project"       : [
+                    PROJECT_REPO        : env.PROJECT_REPO,
+                    PR_NUM              : env.PR_NUM,
+                    AUTHOR_REPO_URL     : env.AUTHOR_REPO_URL,
+                    SOURCE_BRANCH       : env.SOURCE_BRANCH,
+            ],
+            ]
+
+    // Get the map of env var keys and values and write them to the env global variable
+    if(stages.containsKey(stage)) {
+        stages.get(stage).each { key, value ->
+            env."${key}" = value
+        }
+    }
+}
+
+/**
+ * Wrap the pipeline with timestamps and ansiColor
+ * @param body Pipeline goes in here
+ */
+def ciPipeline(Closure body) {
+    ansiColor('xterm') {
+        deleteDir()
+        body()
+    }
+}
+
+def handlePipelineStep(Map config, Closure body) {
+    try {
+
+        if (config.debug) {
+            echo "Starting ${config.stepName}"
+        }
+
+        body()
+
+    } catch (Throwable err) {
+
+        echo err.getMessage()
+        throw err
+
+    } finally {
+
+        if (config.debug) {
+            echo "end of ${config.stepName}"
+        }
+    }
+}
+
+
+/**
+ * Function to return the job name
+ * @return
+ */
+def timedMeasurement() {
+    return env.JOB_NAME
 }
---
+++ test/config/Dockerfiles/contra-env-sample-project-test-c7/prepare_and_test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+base_dir=/home
+
+# Set log dir to the WORKSPACE where it can be archived
+if [ ! -z "${WORKSPACE}" ]; then
+    log_dir=${WORKSPACE}
+else
+    log_dir=${base_dir}/logs
+fi
+
+# Set environment to use ara with ansible
+export ara_location=$(python -c "import os,ara; print(os.path.dirname(ara.__file__))")
+export ANSIBLE_CALLBACK_PLUGINS=$ara_location/plugins/callbacks
+export ANSIBLE_ACTION_PLUGINS=$ara_location/plugins/actions
+export ANSIBLE_LIBRARY=$ara_location/plugins/modules
+
+export USER=$(whoami)
+
+cd ${base_dir}
+
+# Prepare repo and logs directories
+mkdir -p ${log_dir}
+
+git clone https://github.com/CentOS-PaaS-SIG/contra-env-setup.git
+
+# Run the playbook locally with added hook for debugging variables needed for testing
+/usr/bin/ansible-playbook -vv -i "localhost," ${base_dir}/contra-env-setup/playbooks/setup.yml -e user=root \
+                          -e ansible_connection=local -e setup_nested_virt=false -e setup_playbook_hooks=true \
+                          -e project_repo=${AUTHOR_REPO_URL} -e project_branch=${SOURCE_BRANCH} \
+                          --extra-vars='{"hooks": ["/home/debug_vars.yml"]}'
+
+# Run tests with pytest
+python -m pytest ${base_dir}/test_contra_env_sample_project.py -v --junitxml=${log_dir}/contra_env_sample_project_centos7.xml > ${log_dir}/contra_env_sample_project_centos7.log
